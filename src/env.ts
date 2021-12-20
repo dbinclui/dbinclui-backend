c8d2ad932c4b8f93a35374dd42e0297d64511e6c
arthurvergacas <arthurvdm@gmail.com>
2021-12-20 14:56:00 -0300

    chore: polish choice of .env file

--- src/env.ts
+++ src/env.ts
@@ -2,8 +2,17 @@
 import path from 'path';
 import dotenv from 'dotenv';
 
-(async () => {
-  const enviromentFileName = process.env.NODE_ENV === 'development' ? '.env.dev' : '.env.prod';
+(() => {
+  if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'development') {
+    console.error(`
+    Variável de ambiente NODE_ENV não está definida corretamente.
+    Certifique-se que a variável NODE_ENV está definida como 'development' ou 'production' na sua máquina.
+    Para mais informações, veja: https://stackoverflow.com/questions/11104028/why-is-process-env-node-env-undefined
+  `);
+    return;
+  }
+
+  const enviromentFileName = process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev';
 
   try {
     const result = dotenv.config({
@@ -15,46 +24,8 @@ import dotenv from 'dotenv';
     }
   } catch {
     console.error(`
-        Ocorreu um erro ao configurar o caminho para o arquivo '.env.dev'.
-        Confirme que a pasta enviroments está configurada na raiz do projeto contendo o arquivo '.env.dev'. 
+        Ocorreu um erro ao carregar o arquivo contendo as variáveis de ambiente.
+        Confirme que a pasta enviroments está na raiz do projeto e contém os arquivos necessários. 
       `);
   }
 })();
-
-if (process.env.NODE_ENV === 'development') {
-  try {
-    const result = dotenv.config({
-      path: path.resolve(process.cwd(), 'environments', '.env.dev'),
-    });
-
-    if (result.error) {
-      throw result.error;
-    }
-  } catch {
-    console.error(`
-      Ocorreu um erro ao configurar o caminho para o arquivo '.env.dev'.
-      Confirme que a pasta enviroments está configurada na raiz do projeto contendo o arquivo '.env.dev'. 
-    `);
-  }
-} else if (process.env.NODE_ENV === 'production') {
-  try {
-    const result = dotenv.config({
-      path: path.resolve(process.cwd(), 'environments', '.env.prod'),
-    });
-
-    if (result.error) {
-      throw result.error;
-    }
-  } catch {
-    console.error(`
-      Ocorreu um erro ao configurar o caminho para o arquivo '.env.prod'.
-      Confirme que a pasta enviroments está configurada na raiz do projeto contendo o arquivo '.env.prod'. 
-    `);
-  }
-} else {
-  console.error(`
-    Variável de ambiente NODE_ENV não está definida corretamente.
-    Certifique-se que a variável NODE_ENV está definida como 'development' ou 'production' na sua máquina.
-    Para mais informações, veja: https://stackoverflow.com/questions/11104028/why-is-process-env-node-env-undefined
-  `);
-}
