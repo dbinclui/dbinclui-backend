d83c2359054a1e6ad93097a2abf8ae10579c7ee7
Eduardo Felipe Araya <eduardojezine@gmail.com>
2021-12-19 22:20:09 -0300

    [feature] Adding new workflow and configs to jest

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from '@jest/types';
+
+export const config = async (): Promise<Config.InitialOptions> => ({
+  verbose: true,
+  preset: 'ts-jest/presets/default',
+  name: 'dbinclui',
+  rootDir: '.',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,js}',
+    '!/node_modules/**',
+    '!/coverage/**',
+  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  modulePaths: [
+    '<rootDir>/src/**/*.{spec,test}.{ts,js}',
+    '<rootDir>/tests/**/*.{spec,test}.{ts,js}',
+  ],
+});
+
+export default config;
