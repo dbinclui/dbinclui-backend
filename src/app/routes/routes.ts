6c5ce9e74e8e2267a1c64b685401f939365ca509
Eduardo Felipe Araya <eduardojezine@gmail.com>
2021-12-22 15:45:23 -0300

    [feature]

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,14 +3,14 @@ import GuidesController from '../controllers/GuidesController';
 
 const router = Router();
 
+router.get('*', (req, res) => {
+  res.status(404).json({ message: 'Ish, nada aqui O.o' });
+});
+
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Bem vindo a API DBInclui' });
 });
 
 router.get('/accessibility-guide', GuidesController.getGuides);
-
-router.get('*', (req, res) => {
-  res.status(404).json({ message: 'Ish, nada aqui O.o' });
-});
 
 export default router;
