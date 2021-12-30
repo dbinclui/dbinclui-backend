4b29a40622edf2d064164b802c969e676f1e0007
fabiojcbatista <fabiojcbweb@gmail.com>
2021-12-29 21:07:19 -0300

    [refactor] added tests.

--- tests/unit/app/validator/GuidesValidator.test.ts
+++ tests/unit/app/validator/GuidesValidator.test.ts
@@ -1,61 +1,73 @@
 import { getMockReq, getMockRes } from '@jest-mock/express';
+import { validationResult } from 'express-validator';
 import { Guides } from '../../../../src/app/entities/guides';
-import { registerValidate, validateRequestSchema } from '../../../../src/app/middlewares/validator/GuidesValidator';
+import {
+  registerValidate,
+  validateRequestSchema,
+} from '../../../../src/app/middlewares/validator/GuidesValidator';
 
 jest.useFakeTimers();
 
 jest.mock('../../../../src/app/middlewares/validator/GuidesValidator');
 
-const validateRequestSchemaMock = validateRequestSchema as jest.MockedFunction<typeof validateRequestSchema>;
+const validateRequestSchemaMock = validateRequestSchema as jest.MockedFunction<
+  typeof validateRequestSchema
+>;
+const validationResultMock = validationResult as jest.MockedFunction<typeof validationResult>;
 const registerValidateMock = registerValidate as jest.Mocked<typeof registerValidate>;
 
-describe(validateRequestSchema.name, () => {
+describe('GuidesValidator Test', () => {
   const registerValidateListMock: Guides[] = [
-   {
-     title: 'teste jest',
-     content: 'testando o test',
-   },
-   {
-     title: 'teste2222 jest',
-     content: 'testando o test2222',
-   },
- ];
+    {
+      title: 'teste jest',
+      content: 'testando o test',
+    },
+    {
+      title: 'teste2222 jest',
+      content: 'testando o test2222',
+    },
+  ];
 
- beforeEach(() => {
-   validateRequestSchemaMock.mockClear();
- });
- 
-  it(`${validateRequestSchemaMock.name}: 
-   the method is called should`, () => {
-   const req = getMockReq();
-   const { res } = getMockRes();
-   const callback = jest.fn();
-   validateRequestSchemaMock(req, res, callback);
-   expect(validateRequestSchemaMock).toHaveBeenCalled();
-  
+  beforeEach(() => {
+    validateRequestSchemaMock.mockClear();
   });
 
-/*  
   it(`${validateRequestSchemaMock.name}: 
-  the method is called should`, () => {
-   const req = getMockReq();
-   const { res } = getMockRes();
-   const callback = jest.fn();
+   when the method is called return ok`, () => {
+    const req = getMockReq();
+    const { res } = getMockRes();
+    const callback = jest.fn();
+    validateRequestSchemaMock(req, res, callback);
+    expect(validateRequestSchemaMock).toHaveBeenCalled();
+  });
 
-   const errorMessage = 'Error';
-   validateRequestSchemaMock.mockImplementationOnce(async () =>
-   Promise.reject(errorMessage),
- );
+  it(`${validateRequestSchemaMock.name}: 
+  when the method validateRequestSchemaMock is called return error 405 and jason response or NextFunction`, () => {
+    const req = getMockReq();
+    const { res } = getMockRes();
+    const callback = jest.fn();
 
-   validateRequestSchemaMock(req, res, callback);
-   expect(validateRequestSchemaMock).toHaveBeenCalled();
-   expect(res.status).toHaveBeenCalledWith(405);
-   expect(res.json).toHaveBeenCalledWith(
-     expect.objectContaining({
-       errors: errorMessage,
-     }),
-   );
- });
-*/
+    const errorMessage = 'Error';
 
+    const validateMock = jest.fn().mockImplementation(() => ({
+      isEmpty: () => false,
+    }));
+
+    const result = validationResultMock(req);
+    expect(!result).toEqual(false);
+
+    if (!result.isEmpty()) {
+      validateRequestSchemaMock(req, res, callback);
+      expect(validateRequestSchemaMock).toBeCalled();
+      expect(validateRequestSchemaMock).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(405);
+      expect(res.json).toHaveBeenCalledWith(
+        expect.objectContaining({
+          errors: errorMessage,
+        }),
+      );
+    }
+
+    expect(callback).toBeTruthy();
+  });
 });
