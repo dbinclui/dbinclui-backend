8f0807b388673ada526abcfe8cb98a14eae851a8
mgunnar <devmatheus@gmail.com>
2022-01-07 15:48:59 -0300

    Ajustando entidade para categorias com prettier

--- src/app/entities/categories.ts
+++ src/app/entities/categories.ts
@@ -1,16 +1,10 @@
 import { ObjectId } from 'mongoose';
-import {Guides } from './guides';
+import { Guides } from './guides';
 
 export interface Categories {
-
   _id: ObjectId;
-  _id_Guides:  ObjectId;
+  _idGuides: ObjectId;
+  _idParent?: ObjectId;
   title: string;
-  content: string;
-  }
-
-/** 
-Código: Deve ser numérico e sequencial, único no sistema - chave primária; - obrigatório
-Guia: campo de seleção para escolher o guia a que esta categoria pertence. - obrigatório
-Categoria: descrição curta da categoria que está sendo criada. - obrigatório
-Descrição: texto para descrição da categoria criada.*/
+  shortDescription: string;
+}
