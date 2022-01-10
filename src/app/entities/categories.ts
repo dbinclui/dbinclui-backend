67de18bd770f857fcef78f659c23b110efe85c2c
mgunnar <devmatheus@gmail.com>
2022-01-10 14:11:06 -0300

    adicionando _id que havia *sumido* no commit anterior

--- src/app/entities/categories.ts
+++ src/app/entities/categories.ts
@@ -2,6 +2,7 @@ import { ObjectId } from 'mongoose';
 import { Guides } from '@entities/guides';
 
 export interface Categories {
+  _id?: ObjectId;
   title: string;
   shortDescription: string;
   guide: Guides;
