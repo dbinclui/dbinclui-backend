a2b84c740b66678290b20ad86a64fbdfcf34629e
mgunnar <devmatheus@gmail.com>
2022-01-10 11:26:33 -0300

    Rodando o prettier.

---
+++ src/app/models/categories.ts
@@ -0,0 +1,16 @@
+import { model, Schema, SchemaTypes, ObjectId } from 'mongoose';
+import { Categories } from '@entities/categories';
+import { Guides } from '@entities/guides';
+
+const CategoriesSchema = new Schema<Categories>(
+  {
+    title: String,
+    shortDescription: String,
+    guide: { type: Schema.Types.ObjectId, ref: 'Guides' },
+    parentCategory: { type: Schema.Types.ObjectId, ref: 'Categories' }, // Acredito que dê pra referênciar a própria tabela assim
+  },
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
+);
+
+export const CategoriesModel = model<Categories>('Categories', CategoriesSchema, 'categories');
+export default CategoriesModel;
