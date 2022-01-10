134eaed9f88b12070d79bf7f5675b8ff78c92620
nickolasL13 <nickk.lima17@gmail.com>
2022-01-10 18:16:14 -0300

    [bugfix] DBI-US003-37

--- src/app/routes/guides-router.ts
+++ src/app/routes/guides-router.ts
@@ -10,8 +10,9 @@ export default (router: Router) => {
     router.get('/list', GuidesController.getGuides),
     router.post(
       '/register',
-      registerValidate(), validateRequestSchema,
-      GuidesController.registerGuide,                                                                              
+      registerValidate(),
+      validateRequestSchema,
+      GuidesController.registerGuide,
     ),
   ]);
 };
