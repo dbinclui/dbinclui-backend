70e7bd1c4e35b9891beee89ec10b62f24f7a2796
ysncastro <yasmincastrosza@gmail.com>
2022-01-12 11:19:00 -0300

    criar model digitalContent

---
+++ src/app/models/digitalContent.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from 'mongoose';
+import { digitalContent } from '../entities/digitalContent';
+
+const DigitalContentSchema = new Schema<digitalContent>(
+    {
+        title: { type: String, required: true},
+        shortDescription: {type: String, required: true},
+        guide: { type: Schema.Types.ObjectId, ref: 'Guides', required: true },
+        category: { type: Schema.Types.ObjectId, ref: 'Category' },
+        // eslint-disable-next-line no-undef
+        filePath: {type: String, required: true}
+    }
+);
+
+
+
+export const digitalContentModel = model<digitalContent>('DigitalContent', DigitalContentSchema, 'digitalContent');
+export default digitalContentModel;
