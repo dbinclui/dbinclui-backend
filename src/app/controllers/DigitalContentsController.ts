80fb2c9d5f63866288ad5ce0da8eafeec3a6bffe
arthurvergacas <arthurvdm@gmail.com>
2022-01-17 11:32:16 -0300

    [feature] Abstrair bind de instância em função

--- src/app/controllers/DigitalContentsController.ts
+++ src/app/controllers/DigitalContentsController.ts
@@ -61,4 +61,16 @@ export class DigitalContentsController {
   }
 }
 
-export default new DigitalContentsController();
+const bindedInstance = <T>(ClassObject: new (...args: any[]) => T): T => {
+  const instance: { [key: string]: any } = new ClassObject();
+
+  Object.getOwnPropertyNames(ClassObject.prototype).forEach((method) => {
+    if (instance[method].bind) instance[method] = instance[method].bind(instance);
+  });
+
+  return instance as T;
+};
+
+const instance = bindedInstance(DigitalContentsController);
+
+export default instance;
