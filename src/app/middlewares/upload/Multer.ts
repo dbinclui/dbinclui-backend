2e2b54b60c057e6969be6f34032b0d5f928c3d1d
fabiojcbatista <fabiojcbweb@gmail.com>
2022-01-18 18:20:11 -0300

    Ajustes no tamanho de arquivo e tipo suportado

--- src/app/middlewares/upload/Multer.ts
+++ src/app/middlewares/upload/Multer.ts
@@ -16,6 +16,21 @@ const storage = new CloudinaryStorage({
   } as any,
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  fileFilter: (req, file, callback) => {
+    const validTypes = /video|image/;
+
+    if (validTypes.test(file.mimetype)) {
+      return callback(null, true);
+    }
+    callback(null, false);
+    return callback(new Error('Arquivo não suportado. Envie apenas vídeo ou imagem.'));
+  },
+
+  storage,
+  limits: {
+    fileSize: 5000000,
+  },
+});
 
 export default upload;
