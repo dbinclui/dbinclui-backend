6ec56421bdf1d2607625ca348046a0e532053a50
LizSantana <liz.santanabrito@gmail.com>
2022-01-26 18:41:31 -0300

    retirando testes do validateSchema do guidesValidator.test

--- tests/unit/app/validator/GuidesValidator.test.ts
+++ tests/unit/app/validator/GuidesValidator.test.ts
@@ -1,7 +1,6 @@
 import { getMockReq, getMockRes } from '@jest-mock/express';
 import { validationResult, body, ValidationChain, Result } from 'express-validator';
 import { guidesValidate } from '@middlewares/validator/GuidesValidator';
-import { validateRequestSchema } from '@middlewares/validator/ValidateSchema';
 
 jest.useFakeTimers();
 
@@ -38,53 +37,5 @@ describe('GuidesValidator Test', () => {
     expect(validatSchemaChainMock.isString).toBeCalledTimes(2);
     expect(bodyMock).toBeCalledWith('title');
     expect(bodyMock).toBeCalledWith('content');
-  });
-
-  it(`${validateRequestSchema.name}: 
-  when the body is invalid should return response with status and error content`, () => {
-    const status = 405;
-    const errorsMessage = ['Invalide title', 'Invalid content'];
-    const req = getMockReq();
-    const isEmpty = jest.fn();
-    const errorsArray = jest.fn();
-    isEmpty.mockReturnValue(false);
-    errorsArray.mockReturnValue(errorsMessage);
-    validationResultMock.mockImplementation(
-      () =>
-        ({
-          isEmpty,
-          array: errorsArray,
-        } as unknown as Result),
-    );
-    const { res, next } = getMockRes();
-
-    validateRequestSchema(req, res, next);
-    expect(validationResultMock).toBeCalled();
-    expect(isEmpty).toBeCalledTimes(1);
-    expect(next).not.toBeCalled();
-    expect(res.status).toBeCalledWith(status);
-    expect(res.json).toBeCalledWith(
-      expect.objectContaining({
-        errors: errorsMessage,
-      }),
-    );
-  });
-
-  it(`${validateRequestSchema.name}: 
-   when the body is valid should call next`, () => {
-    const req = getMockReq();
-    const isEmpty = jest.fn();
-    validationResultMock.mockImplementation(
-      () =>
-        ({
-          isEmpty,
-        } as unknown as Result),
-    );
-    isEmpty.mockReturnValue(true);
-    const { res, next } = getMockRes();
-    validateRequestSchema(req, res, next);
-    expect(validationResultMock).toBeCalled();
-    expect(isEmpty).toBeCalledTimes(1);
-    expect(next).toBeCalledTimes(1);
   });
 });
