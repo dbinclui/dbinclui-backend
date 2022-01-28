d6e56457d271a5f7f7b9b01b2787903346fa5286
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2022-01-28 16:02:32 -0300

    rota para consultDigitalContent Co-authored-by: Luh-Santos <Luh-Santos@users.noreply.github.com>

--- src/app/routes/Digital-contents-router.ts
+++ src/app/routes/Digital-contents-router.ts
@@ -3,11 +3,13 @@ import { registerValidate } from '@middlewares/validator/DigitalContentValidator
 import DigitalContentsController from '@controllers/DigitalContentsController';
 import { validateRequestSchema } from '@middlewares/validator/ValidateSchema';
 import upload from '@middlewares/upload/Multer';
+import digitalContentRepository from '@repositories/DigitalContentsRepository';
 
 const router = Router();
 
 export default [
   router.get('/list', DigitalContentsController.getDigitalContents),
+  router.get('/:id', DigitalContentsController.consultDigitalContent),
   router.post(
     '/register',
     upload.array('files'),
