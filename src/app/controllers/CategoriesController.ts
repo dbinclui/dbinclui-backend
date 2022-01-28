0937ab3ca8995128ecbdae55a8c390b922e73cb5
arthurvergacas <arthurvdm@gmail.com>
2022-01-28 10:23:14 -0300

    [fix] Ajustar códigos http do CategoriesController

--- src/app/controllers/CategoriesController.ts
+++ src/app/controllers/CategoriesController.ts
@@ -14,7 +14,7 @@ export class CategoriesController {
       const categories = await this.repository.list();
       res.status(200).json({ data: categories });
     } catch (error) {
-      res.status(400).json({
+      res.status(500).json({
         message: error,
       });
     }
@@ -45,20 +45,11 @@ export class CategoriesController {
   async registerCategory(req: Request, res: Response) {
     try {
       const categories = await this.repository.create(req.body);
-      res.status(200).json({ data: categories });
+      res.status(201).json({ data: categories });
     } catch (error) {
-      res.status(400).json({
+      res.status(500).json({
         message: error,
       });
-    }
-  }
-
-  async consultCategories(req: Request, res: Response) {
-    try {
-      const categories = await this.repository.getById(req.params.id);
-      res.status(200).json({ data: categories });
-    } catch (error) {
-      res.status(500).json({ message: error });
     }
   }
 }
